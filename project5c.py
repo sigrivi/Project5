e5a1910f4604c8f754f697e77540e8871d68ccdd
sigrivi <sigrivi@uio.no>
2017-12-10 15:17:09 +0100

    some changes

--- project5c.py
+++ project5c.py
@@ -6,9 +6,14 @@ import scipy.sparse as sps
 import time
 import random
 from multiprocessing import Pool,cpu_count
+import sys
+
+# this is a program for solving the 1D diffuion equation.
+# there are three different schemes: explicit, implicit and crank nicolson
 
 
-def makeA(Nx,alpha): # to make the tridiagonal matrix
+# to make the tridiagonal matrix of the implicit solver and the crank nicolson solver
+def makeA(Nx,alpha): 
 	A = np.identity(Nx)*(1+2*alpha)
 	for i in range(Nx-1):
 		A[i,i+1] = -alpha
@@ -16,100 +21,165 @@ def makeA(Nx,alpha): # to make the tridiagonal matrix
 
 	return(A)
 
-def rowreduction(A,v):
-	for i in range(A.shape[0]-1): # Gaussian elimination
-		coeff=A[i+1,i]/A[i,i]
-		A[i+1,i] = 0
-		A[i+1,i+1]=A[i+1,i+1]-A[i,i+1]*coeff
-		v[i+1]=v[i+1]-v[i]*coeff
-	# return(A,v)
+# to do row reduction on the matrix equation of the implicit solver and the crank nicolson
+# in place modification of A and u
+def rowreduction(A,u): 
+	for i in range(A.shape[0]-1):                                             # Gaussian elimination
+		coeff = A[i+1,i]/A[i,i]
+		A[i+1,i+1] = A[i+1,i+1]-A[i,i+1]*coeff
+		u[i+1] = u[i+1]-u[i]*coeff
+	
 
-def backsubstitution(A,v,solution,alpha):
+# to find the solution of the matrix equation of the implicit solver and the crank nicolson
+def backsubstitution(A, u, alpha): 
 	Nx = A.shape[0]
-	solution[Nx] = 1 # boundary condition
-	for i in range(1,Nx+1): #backward substitution
+	solution = u.copy()
+	solution[Nx] = 1                                      					  # boundary condition
+	for i in range(1,Nx+1):                              					  # backward substitution
 		k = Nx-i
-		solution[k] = (v[k]+alpha*solution[k+1])/A[k,k]
+		solution[k] = (u[k]+alpha*solution[k+1])/A[k,k]
 
 	return(solution)
 
-def forwardstep(u,unew,alpha):
-	unew[0] = (1-2*alpha)*u[0] + alpha*(u[1]) # the boundary u_0 = 1 is not included in the vecror u
+# to make one euler forward step. used in the explicit solver and the crank nicolson
+def forwardstep(u, alpha): 
+	unew = u.copy()
+	unew[0] = (1-2*alpha)*u[0] + alpha*(u[1])                                 # the boundary u_0 = 0 is not included. the vector u starts at u_1. This is to make the solutions of same length for all solvers
 	unew[1:Nx] = (1-2*alpha)*u[1:Nx] + alpha*(u[2:Nx+1]+u[0:Nx-1])
-
-	#return(unew)
-def explisitsolver(Nt,tfinal,Nx,frequency):
-	alpha = Nx**2*tfinal/Nt # alpha = dt/dx**2 =(tfinal/Nt)/(1/Nx)**2
-	u = np.zeros(Nx+1)
-	u[Nx] = 1
-	unew = np.zeros(Nx+1)
-	unew[Nx] = 1
-	U = np.zeros((Nx+2,Nt//frequency))
-	counter = 0
-	for j in range(Nt):
-		forwardstep(u,unew,alpha)
 	
-		u = unew.copy()
-		u[Nx] = 1
-		if j%frequency == 0:
-			U[1:,counter] = u
+	return(unew)
 
-			counter += 1
-	return(U)
-
-def implistsolver(Nt,tfinal,Nx,frequency):
-	alpha = Nx**2*tfinal/Nt # alpha = dt/dx**2 =(tfinal/Nt)/(1/Nx)**2
-	B = makeA(Nx,alpha)
-	U = np.zeros((Nx+2,Nt//frequency)) # matrix to store solutions in
-	counter = 0
-	v = np.zeros(Nx+1)
-	solution = np.zeros(Nx+1)
-
-	for t in range(Nt):
-		v[Nx] = 1 # boundary condition
-		A = B.copy()
-
-		rowreduction(A,v)
-
-		v = backsubstitution(A,v,solution,alpha)
-
-		if t%frequency == 0: # to store every 1/frequency solution
-			U[1:,counter] = v
-			counter += 1
-	return(U)
-
-def cranknicolson(Nt,tfinal,Nx,frequency):
-	beta = Nx**2*tfinal/(2*Nt)
+# to initialize the variables which are common for all the solvers
+def initialize(Nt,tfinal,Nx,frequency): 
+	alpha = Nx**2*tfinal/Nt           	                                      # alpha = dt/dx**2 =(tfinal/Nt)/(1/Nx)**2
 	u = np.zeros(Nx+1)
-	solution = np.zeros(Nx+1)
 	u[Nx] = 1
-	unew = np.zeros(Nx+1)
-	unew[Nx] = 1
 	U = np.zeros((Nx+2,Nt//frequency))
-	B = makeA(Nx,beta)
 	counter = 0
+	
+	return(alpha,u,U,counter)
+
+# solves the 1D diffusion equation
+def explicitsolver(Nt,tfinal,Nx,frequency): 
+	alpha,u,U,counter = initialize(Nt,tfinal,Nx,frequency)
+
+	for j in range(Nt):
+		u = forwardstep(u, alpha)
+
+		if j%frequency == 0:                                                  # to store every Nt/frequency solution
+			U[1:,counter] = u
+			counter += 1
+
+	return(U)
+
+# solves the 1D diffusion equation
+def implictsolver(Nt,tfinal,Nx,frequency): 
+	
+	alpha,u,U,counter = initialize(Nt,tfinal,Nx,frequency)
+	B = makeA(Nx,alpha)
+
 	for t in range(Nt):
-		forwardstep(u,unew,beta)
+
 		A = B.copy()
-		#u[Nx] = 1
-		rowreduction(A,unew)
+		rowreduction(A, u)
 
-		u = backsubstitution(A,unew,solution,beta)
+		u = backsubstitution(A, u, alpha)
 
-		if t%frequency == 0: # to store every 1/frequency solution
+		if t%frequency == 0: 				                                  # to store every frequency solution
 			U[1:,counter] = u
 			counter += 1
 	return(U)
 
-Nt = 1000
+# solves the 1D diffusion equation
+def cranknicolson(Nt,tfinal,Nx,frequency): 
+
+	alpha,u,U,counter = initialize(Nt,tfinal,Nx,frequency)
+	beta = alpha/2                                                            # for crank nicolson, alpha is half of alpha in the other solvers
+	B = makeA(Nx,beta)
+
+	for t in range(Nt):
+		u = forwardstep(u, beta)
+
+		A = B.copy()
+		rowreduction(A, u)
+
+		u = backsubstitution(A, u, beta)
+
+		if t%frequency == 0:                                                  # to store every frequency solution
+			U[1:,counter] = u
+			counter += 1
+	return(U)
+
+# makes the analytical solution, which is a sum of fourier coefficients
+def exactsolution(x,t): 
+
+	u    = np.zeros(len(x))	
+	k = 1
+	norm = 1
+		
+	while norm> 1e-15:                                                        # here you can set the precision of the analytical solution
+		
+		kpi  = np.pi *k
+		uold = u.copy()
+
+		if k%2 == 0:
+			u += 2*np.exp(-(kpi)**2 *t)*np.sin(kpi*x)/(kpi)
+		else:
+			u += -2*np.exp(-(kpi)**2 *t)*np.sin(kpi*x)/(kpi)
+		k +=1
+		norm = np.linalg.norm(u-uold)
+		
+	u += x
+	return(u)
+
+# to plot the solutions. Inputs are solution vectors, and the time at which the solution is calculated
+def plotsolutions(ue,ui,uc, t): 
+
+	Nx = len(ue)
+	x = np.linspace(0,1,num = 1000)
+	exact = exactsolution(x,t)
+	axis = np.linspace(0,1,num = Nx)
+
+	plt.plot( x, exact, label = 'exact')
+	plt.plot(axis,ue, label = 'explicit')
+	plt.plot(axis,ui, label = 'implict')
+	plt.plot(axis,uc, label = 'cranknicolson')
+	plt.legend()
+	plt.xlabel('x')
+	plt.ylabel('u')
+	plt.title('u(x,t = %.3f) '%t )
+	#plt.savefig('Nt150Nx10t02', dpi = 225)
+	plt.show()
+
+def calculateerror(v,u): # to calculate the relative error. v is the exact solution, u is the numerical solution
+	error = np.linalg.norm(v-u)/np.linalg.norm(v)
+	return(error)
+
+# set the variables here ------------------------------------------------------
+Nt = 2000
 tfinal = 1.
+frequency = 10 	                                                              # Nt/frequency  is the number of solutions
+n = 4				                                                          # solution number n
 Nx = 10
-frequency = 10
+t = n*frequency/Nt 	                                                          # time 
 axis = np.linspace(0,1,num = Nx+2)
 
-U = explisitsolver(Nt,tfinal,Nx,frequency)
-#U = implistsolver(Nt,tfinal,Nx,frequency)
-#U = cranknicolson(Nt,tfinal,Nx,frequency)
+# make the solutions-----------------------------------------------------------
+exact = exactsolution(axis,t)
+Ue = explicitsolver(Nt,tfinal,Nx,frequency)
+Ui = implictsolver(Nt,tfinal,Nx,frequency)
+Uc = cranknicolson(Nt,tfinal,Nx,frequency)
 
-plt.plot(axis,U[:,2],axis,U[:,10], axis,U[:,20], axis,U[:,50])
-plt.show()
+#to calculate the error and to check the benchmarks:---------------------------
+print('errorE',calculateerror(exact,Ue[:,n]))
+print('errorI',calculateerror(exact,Ui[:,n]))
+print('errorC',calculateerror(exact,Uc[:,n]))
+
+print('Ue', Ue[:,n] )
+print('Ui', Ui[:,n] )
+print('Uc', Uc[:,n] )
+
+plotsolutions(Ue[:,n], Ui[:,n], Uc[:,n],t)
+
+
+
--- project5f.py
+++ project5f.py
@@ -6,74 +6,85 @@ import scipy.sparse as sps
 import time
 import random
 from multiprocessing import Pool,cpu_count
+import sys
 
 import matplotlib.cm as cm
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
+# The explicitsolver2D solves the 2D diffusion equation. 
+# initial conditions and boundaries are set manually inside the function
+# you choose the number of time step, and number og grid points
+# the function returns solution number n*frequency and a vector representing the x- and y axis
 
-Nt = 10000
-tfinal = 1.
-Nx = 20
-frequency = 100
-n = 2
-t = n*frequency/Nt 	# time 
-
-
-def explicitsolver(Nt,tfinal,Nx,frequency,n):
+def explicitsolver2D(Nt,tfinal,Nx,frequency,n):
 	
 	U = np.zeros((Nx+1,Nx+1))
-
 	alpha = Nx**2*tfinal/Nt
 	axis = np.linspace(0,1,num = Nx+1)
 	
+	
 # initial conditions: u(x,y,0)=sin(pi*x)sin(pi*y) boundaries: u(0,y,t)=u(1,y,t)=u(x,0,t)=u(x,1,t)
 	x0 = np.sin(axis *np.pi)
-	for i in range(Nx):
-		for j in range(Nx):
-			U[i,j]=x0[i]*x0[j]
+	np.outer(x0, x0, U)
 	
 # initial conditions: u(x,y,0)=0 for 0<x<1 and 0<y<1 boundaries: u(1,y,t)=u(x,1,t)=1
 	#U[Nx,:] = np.ones(Nx+1)
 	#U[:,Nx] = np.ones(Nx+1)	
-	
-	Unew = U.copy()
 
 	for l in range(Nt):
 
-		for i in range(1,Nx):
-			for j in range(1,Nx):
+		U[1:Nx,1:Nx] = alpha*(U[2:Nx+1,1:Nx] + U[0:Nx-1,1:Nx] + U[1:Nx,2:Nx+1] + U[1:Nx,0:Nx-1] - 4*U[1:Nx,1:Nx]) + U[1:Nx,1:Nx]
 
-				Unew[i,j] = alpha*(U[i+1,j]+U[i-1,j]+U[i,j+1]+U[i,j-1]-4*U[i,j])+U[i,j]
-		U = Unew.copy()
-
-		if l%frequency == 0: # plots every frequency solution
-			plt.plot(axis, U[:,10])
-
-		if l == n*frequency:
+		if l == n*frequency:                                                  # the function returns only one solution, solution n*frequency
 			Ureturn = U.copy()
-			
+
+ #to make the cross section plot:
+		if l%frequency == 0:                                                  # plots every frequency solution
+			ax = plt.plot(axis, U[:,10])
+			plt.plot(0.5,np.exp(-2*np.pi**2*l/Nt),'bo')
+
 	plt.xlabel('x')
 	plt.ylabel('u(x,0.5,t)')
 	plt.title('A cross section of the plane')
+	plt.savefig('crosssection' , dpi = 225)
 	plt.show()
+
 	return(Ureturn,axis)
 
+# set the variables here ------------------------------------------------------
+Nt = 100000
+tfinal = 1.
+Nx = 20
+frequency = 1000
+n = 4
+t = n*frequency/Nt 	# time 
 
-U,axis = explicitsolver(Nt,tfinal,Nx,frequency,n)
+# make the solution -----------------------------------------------------------
+U,axis = explicitsolver2D(Nt,tfinal,Nx,frequency,n)
+
+# to calculate the error and to check the benchmarks: -------------------------
+exact = np.sin(axis *np.pi)*np.exp(-2*np.pi**2*t)                             # u_exact(x,0.5,t)
+print('t',t,'Nx',Nx)
+print('exact',exact)
+print('numerical',U[:,Nx//2])
+error = np.linalg.norm(exact -U[:,Nx//2] )/np.linalg.norm(exact)              # relative error
+print('error',error)
 
 
+# to make contour plots of the solution ---------------------------------------
 X, Y = np.meshgrid(axis, axis)
 plt.figure()
-CS = plt.contour(X, Y, U)
+CS = plt.contour(X, Y, U,np.linspace(0.1,0.9,9))
 plt.clabel(CS, inline=1, fontsize=10)
 plt.title('Contour plot of u(x,y,%.3f)' %t)
 plt.xlabel('x')
 plt.ylabel('y')
-#plt.savefig("diffusion", dpi=225)
+plt.savefig("contour40", dpi=225)
 plt.show()
 
 
+# this is only a sketch of the implicit solver. it does not work
 def implicitsolver(Nt,tfinal,Nx,frequency):
 		#initial conditions
 	U = np.zeros((Nx+1,Nx+1))
@@ -84,18 +95,8 @@ def implicitsolver(Nt,tfinal,Nx,frequency):
 	beta = 1/(1+4*alpha)
 	counter = 0
 	for l in range(Nt):
-
-		Unew[Nx,:] = np.ones(Nx+1)
-		Unew[:,Nx] = np.ones(Nx+1)
-
-		for i in range(1,Nx):
-			for j in range(1,Nx):
-
-				Unew[i,j] = beta*(alpha*(U[i+1,j]+U[i-1,j]+U[i,j+1]+U[i,j-1])+U[i,j])
-				U = Unew
-
-		Unew[0,:] = np.zeros(Nx+1)
-		Unew[:,0] = np.zeros(Nx+1)
+		for i in range(10):
+			U[1:Nx,1:Nx] = beta*(U[2:Nx+1,1:Nx] + U[0:Nx-1,1:Nx] + U[1:Nx,2:Nx+1] + U[1:Nx,0:Nx-1] ) + U[1:Nx,1:Nx]
 
 		if l%frequency == 0: # to store every 1/frequency solution
 			x = axis
@@ -106,7 +107,7 @@ def implicitsolver(Nt,tfinal,Nx,frequency):
 			plt.clabel(CS, inline=1, fontsize=10)
 			plt.title('time = %d' %l)
 			plt.savefig("diffusion%d" %counter, dpi=225)
-			#plt.show()
+			plt.show()
 
 			counter += 1
 
